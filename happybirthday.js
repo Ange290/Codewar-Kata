2199b6204c84d288cbb37e217474db14f9b0ed23
Ange290 <uange209@gmail.com>
2025-08-23 13:14:28 +0200

    Happy Birthday, Darling!

---
+++ happybirthday.js
@@ -0,0 +1,28 @@
+// As you may know, once some people pass their teens, they jokingly only celebrate their 20th or 21st birthday,
+//  forever. With some maths skills, that's totally possible - you only need to select the correct number base!
+
+// For example, if they turn 32, that's exactly 20 - in base 16... Already 39? That's just 21, in base 19!
+
+// Your task is to translate the given age to the much desired 20 (or 21) years, and indicate the number base,
+//  in the format specified below.
+
+// Note: input will be always > 21
+
+// Examples:
+// 32  -->  "32? That's just 20, in base 16!"
+// 39  -->  "39? That's just 21, in base 19!"
+
+function womensAge(n) {
+  if(n <21){ return 'Invalid Input'}
+
+
+  for(let base = 2; base <100; base++){
+    if(n === 2 * base){
+      return `${n}? That's just 20, in base ${base}!`
+    }
+    else if(n === 2 * base + 1){
+      return `${n}? That's just 21, in base ${base}!`
+    }
+}
+ 
+}
