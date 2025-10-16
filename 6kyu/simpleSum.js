5c45107fd40c18a06fef40c1eb62ab093128138a
Ange290 <uange209@gmail.com>
2025-10-16 17:24:18 +0200

    simple sum of pairs

--- 6kyu/simpleSum.js
+++ 6kyu/simpleSum.js
@@ -8,4 +8,9 @@ function solve(n) {
   let b = n - a;
   const digitSum = num => num.toString().split('').reduce((s, d) => s + +d, 0);
 
+  return digitSum(a) + digitSum(b);
 }
+console.log(solve(29));     // ➜ 11
+console.log(solve(328));    // ➜ 22
+console.log(solve(1000));   // ➜ 28
+console.log(solve(99));     // ➜ 18
