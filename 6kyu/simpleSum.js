2cebd44f67f6c1aac3c0589e8ca56d32118eff81
Ange290 <uange209@gmail.com>
2025-10-16 17:23:55 +0200

    find large

--- 6kyu/simpleSum.js
+++ 6kyu/simpleSum.js
@@ -1,5 +1,11 @@
 function solve(n) {
   let a = 0;
-  let str = n.toString(); 
+  let str = n.toString();
+  
+  for (let i = 0; i < str.length - 1; i++) {
+    a = a * 10 + 9;
+  }
+  let b = n - a;
+  const digitSum = num => num.toString().split('').reduce((s, d) => s + +d, 0);
 
 }
