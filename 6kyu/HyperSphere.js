0f91300deb512fb429c7befcd098a0c8c0ea88b4
Ange290 <uange209@gmail.com>
2025-10-27 19:33:34 +0200

    Fis bugs

--- 6kyu/HyperSphere.js
+++ 6kyu/HyperSphere.js
@@ -4,5 +4,8 @@ function isSphere(coords,radius){
        let r = Math.pow(radius,2);
        let check = coords.map((index)=>index == undefined ? 0: Math.pow(index,2))
   let sum = check.reduce((acc,num)=> acc + num,0)
-
+    if(sum <= r){
+    return true
+  }
+  return false;
 }
