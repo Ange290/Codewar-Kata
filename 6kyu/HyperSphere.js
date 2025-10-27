f7d520f5b26b286a3f6aadfb9e05c830ff2bf643
Ange290 <uange209@gmail.com>
2025-10-27 19:33:03 +0200

    Check if is sphere

--- 6kyu/HyperSphere.js
+++ 6kyu/HyperSphere.js
@@ -2,5 +2,7 @@ function isSphere(coords,radius){
     if(!coords) return true
       if(radius == null) return true
        let r = Math.pow(radius,2);
+       let check = coords.map((index)=>index == undefined ? 0: Math.pow(index,2))
+  let sum = check.reduce((acc,num)=> acc + num,0)
 
 }
