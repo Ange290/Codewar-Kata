1bc31c429bbf1b54206068979acf32f8283ec313
Ange290 <uange209@gmail.com>
2025-11-04 20:18:55 +0200

    remove error

--- 7kyu/sumofarr.js
+++ 7kyu/sumofarr.js
@@ -5,4 +5,11 @@ let nonDuplicate =[];
 for(let num of arr){
     map[num] = (map[num] || 0) + 1;
 }
+for(let num in map){
+    if(map[num] <=1){
+        nonDuplicate.push(Number(num))
+    }
+}
+let sum = nonDuplicate.reduce((acc,num)=> acc + num)
+  return sum;
 }
