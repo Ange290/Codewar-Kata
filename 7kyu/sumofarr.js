c17a0e2c2130a8ba9f052ba1d6c70c66f8f6ac14
Ange290 <uange209@gmail.com>
2025-11-04 20:19:25 +0200

    Testing

--- 7kyu/sumofarr.js
+++ 7kyu/sumofarr.js
@@ -13,3 +13,6 @@ for(let num in map){
 let sum = nonDuplicate.reduce((acc,num)=> acc + num)
   return sum;
 }
+console.log(repeats([4,5,7,5,4,8])); //15
+console.log(repeats([9,10,19,13,19,13])); //19
+console.log(repeats([16,0,11,4,8,16,0,11]));
