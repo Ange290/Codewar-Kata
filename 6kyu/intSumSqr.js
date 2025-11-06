ec4fcc3c6c55687cdc169428498f053c5a6d934f
Ange290 <uange209@gmail.com>
2025-11-06 17:12:29 +0200

    Add new feature

--- 6kyu/intSumSqr.js
+++ 6kyu/intSumSqr.js
@@ -2,5 +2,8 @@ const intSumSq = (n,m) => {
      let result =[];
      for( let num =1; num < n; num++){
      const sum = num.toString().split('').map(Number).reduce((a,b)=> a+b,0);
+     if(sum * sum === m){
+      result.push(num)
+    }
      }
 }
