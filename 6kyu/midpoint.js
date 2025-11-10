323138fa2dac05d866521f9d3a900649082723a6
Ange290 <uange209@gmail.com>
2025-11-10 16:08:02 +0200

    Return value

--- 6kyu/midpoint.js
+++ 6kyu/midpoint.js
@@ -4,6 +4,8 @@ if (n.length < 3) return undefined;
   for (let i = 1; i < n.length - 1; i++) {
        const leftSum = n.slice(0, i).reduce((a, b) => a + b, 0);
     const rightSum = n.slice(i + 1).reduce((a, b) => a + b, 0);
-
+ if (leftSum === rightSum) {
+      return i; 
+    }
   }
 }
