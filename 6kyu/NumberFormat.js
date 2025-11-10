01c4b332a023687a5c90be113c5b6580d4641b2b
Ange290 <uange209@gmail.com>
2025-11-10 16:12:12 +0200

    Add testing

--- 6kyu/NumberFormat.js
+++ 6kyu/NumberFormat.js
@@ -1,3 +1,8 @@
 function numberFormat(number) {
   return number.toLocaleString();
 }
+// Example usage:
+console.log(numberFormat(1234567));
+console.log(numberFormat(1000));
+console.log(numberFormat(9876543210));
+console.log(numberFormat(0));
