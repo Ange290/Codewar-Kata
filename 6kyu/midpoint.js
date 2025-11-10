8d5616fded712ef5e8b1e8cf716d0eabfe1f40b4
Ange290 <uange209@gmail.com>
2025-11-10 16:07:36 +0200

    Find sum on both side

--- 6kyu/midpoint.js
+++ 6kyu/midpoint.js
@@ -2,5 +2,8 @@ var midpointSum = function (n) {
 if (n.length < 3) return undefined;
 
   for (let i = 1; i < n.length - 1; i++) {
+       const leftSum = n.slice(0, i).reduce((a, b) => a + b, 0);
+    const rightSum = n.slice(i + 1).reduce((a, b) => a + b, 0);
+
   }
 }
