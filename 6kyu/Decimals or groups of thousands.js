1115774d3eaddc48b88b5ebeadaec65ab85c6021
Ange290 <uange209@gmail.com>
2025-12-17 10:44:08 +0200

    Find sum of number in US style

--- 6kyu/Decimals or groups of thousands.js
+++ 6kyu/Decimals or groups of thousands.js
@@ -4,4 +4,12 @@ let sum =0;
 let us = /^\d{1,3}(,\d{3})*(\.\d+)?$/;
 let uk = /^\d{1,3}(\.\d{3})*(,\d+)?$/;
 
+for(let x =0; x< arr.length; x++){
+    let value = arr[x];
+   if(us.test(value)){
+      
+   sum += Number(value.replace(/,/g,''));    
+} 
+}
+
 }
