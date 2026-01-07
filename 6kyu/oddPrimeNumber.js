4d7399e6809adaa451aa7b4d5d2aa9265ee60d18
Ange290 <uange209@gmail.com>
2026-01-07 18:26:15 +0200

    find the array of prime number

--- 6kyu/oddPrimeNumber.js
+++ 6kyu/oddPrimeNumber.js
@@ -1,3 +1,14 @@
 function oddPrimeNumber(n) {
-    
+     let result =[];
+  let final =[]
+  
+  for(let i =2; i<=n; i++){
+  let isPrime = true;
+   for(let a =2; a<= Math.sqrt(i); a++){
+   if(i % a === 0){ 
+      isPrime = false
+      break;
+   }
+   
+}}
 }
