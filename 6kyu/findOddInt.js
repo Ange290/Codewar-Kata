49a7ca46388902d0a585e2d93bbab13031cc53b3
Ange290 <uange209@gmail.com>
2026-01-13 18:10:01 +0200

    Check odd int and return it

--- 6kyu/findOddInt.js
+++ 6kyu/findOddInt.js
@@ -6,7 +6,11 @@ function findOdd(A) {
     accumulator[currentValue].push(currentValue)
     return accumulator;
   },{});
-
+ for(let j in grp){
+   if(grp[j].length % 2 !== 0){
+     return Number(j);
+   }
+ }
 }
 console.log(findOdd([1,2,2,3,3,3,4,3,3,3,2,2,1]));
 console.log(findOdd([0,1,0,1,0]));
