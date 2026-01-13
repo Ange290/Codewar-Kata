cb23bc4f3d37f29f7807690ebcb4fbd16a85646d
Ange290 <uange209@gmail.com>
2026-01-13 18:07:24 +0200

    getdata and retries time if it fail

--- 6kyu/getData.js
+++ 6kyu/getData.js
@@ -1,23 +1,28 @@
 
 // Write a JavaScript function that fetches data from an API and retries the request a specified number of times if it fails
-async function getData(url,times){
-   
-    let response = await fetch(url);
-    let result = await response.json();
-    
-    // for(let i=0; i<=times; i++){
-         
-        if(response.ok){
-      
-        return result;      
-    // }
-     }else{
-        return `not not`
-     }
-  
+async function getData(url, times) {
+  let retries = 0;
+
+  while (retries < times) {
+    try {
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const result = await response.json();
+      console.log(result); 
+
+    } catch (error) {
+      retries++;
+      console.log(`Request failed. Retrying (${retries}/${times})...`);
+
+      if (retries === times) {
+        console.log(`Failed after ${times} retries: ${error.message}`);
+      }
+    }
+  }
 }
-async function main() {
-      console.log(await getData(`https://jsonholder.typicode.com/post`));
-}
-   
-main();
+
+      getData(`https://jsonplaceholder.typicode.com/posts`,3);
