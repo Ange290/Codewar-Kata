0b091df8f7e470ac3985a3be5892854507e8486f
Ange290 <uange209@gmail.com>
2026-01-19 10:00:00 +0200

    check if projects are valid

--- 8kyu/studentFinalGrade.ts
+++ 8kyu/studentFinalGrade.ts
@@ -2,4 +2,7 @@ const finalGrade = (exam: any, projects: any) => {
    if (exam < 0 || exam > 100) {
         return "Invalid exam grade. It must be between 0 and 100.";
     }
+    if (projects < 0) {
+        return "Invalid number of projects. It cannot be negative.";
+    }
 };
