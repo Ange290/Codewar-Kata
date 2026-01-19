8cba1b9e78a70aa9d7655842bf9ad0208848c1fc
Ange290 <uange209@gmail.com>
2026-01-19 10:00:00 +0200

    checking conditions

--- 8kyu/studentFinalGrade.ts
+++ 8kyu/studentFinalGrade.ts
@@ -6,4 +6,13 @@ const finalGrade = (exam: any, projects: any) => {
         return "Invalid number of projects. It cannot be negative.";
     }
     let result: number | string;
+    if (exam > 90 || projects > 10) {
+        result = 100;
+    } else if (exam > 75 && projects >= 5) {
+        result = 90;
+    } else if (exam > 50 && projects >= 2) {
+        result = 75;
+    } else {
+        result = 0;
+    }
 };
