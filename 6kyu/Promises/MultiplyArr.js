12b728e2c64367b52e35c315c71cd4f273fb940e
Ange Uwizera <154429400+Ange290@users.noreply.github.com>
2026-01-20 18:57:51 +0200

    Multiplying arr with async/await

--- 6kyu/Promises/MultiplyArr.js
+++ 6kyu/Promises/MultiplyArr.js
@@ -1,3 +1,16 @@
-async function processArray(array){
 
+async function processArray(array){
+  for( let a of array){
+    await someAsyncFunction(a);
+    
+  }
 }
+const someAsyncFunction = async(item)=>{
+  return new Promise((resolve,reject) =>{
+    setTimeout(() =>{
+      console.log(item * 2)
+      resolve()
+    }, 1000);
+  })
+}
+console.log(processArray( [1, 2, 3, 4, 5]));
