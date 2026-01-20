f132c6871aa14de719fb4240ee454ef679aab0fd
Ange Uwizera <154429400+Ange290@users.noreply.github.com>
2026-01-20 17:21:49 +0200

    Add console logs for mineLocation function

--- 6kyu/findMine.js
+++ 6kyu/findMine.js
@@ -2,3 +2,5 @@ const findLocation =(field)=>{
   
 }
 
+console.log(mineLocation([ [1, 0, 0], [0, 0, 0], [0, 0, 0] ]))// [0, 0]))
+console.log(mineLocation([ [0, 0, 0], [0, 1, 0], [0, 0, 0] ])) // [1, 1]))
