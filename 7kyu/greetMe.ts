41b32503b97568033126461d2106a223d185f24c
Ange290 <uange209@gmail.com>
2026-01-23 10:00:00 +0200

    add testing value

--- 7kyu/greetMe.ts
+++ 7kyu/greetMe.ts
@@ -2,3 +2,6 @@ const greet = function(name: any) {
     let capital = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
     return `Hello ${capital}!`;
 }
+console.log(greet('riley')); 
+console.log(greet('JACK'));
+console.log(greet('matt'));
