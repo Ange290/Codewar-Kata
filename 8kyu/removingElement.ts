6d01a056306a2956327fddf25b08e27fe5094eb1
Ange290 <uange209@gmail.com>
2026-01-26 10:00:00 +0200

    add testing value

--- 8kyu/removingElement.ts
+++ 8kyu/removingElement.ts
@@ -6,3 +6,4 @@ const removeEveryOther = (arr: any[]) => {
 console.log(removeEveryOther(['Hello', 'Goodbye', 'Hello Again'])); // ['Hello', 'Hello Again']
 console.log(removeEveryOther([1, 2, 3, 4, 5, 6])); // [1, 3, 5]
 console.log(removeEveryOther([[1, 2], [3, 4], [5, 6], [7, 8]])); // [[1, 2], [5, 6]]
+console.log(removeEveryOther([['Goodbye'], ['Hello Again']])); // [['Goodbye']]
