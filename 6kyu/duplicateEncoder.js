051ec0674a437625d9db25b4d7b3d695f6f4e0f7
Ange290 <uange209@gmail.com>
2026-01-29 17:37:17 +0200

    returning value

--- 6kyu/duplicateEncoder.js
+++ 6kyu/duplicateEncoder.js
@@ -3,7 +3,10 @@ word = word.toLowercase();
 let result ='';
 for(let c= 0; c< word.length; c++){
     if(word.indexOf(word[c] !== word.lastIndexOf(word[c]))){
-
+result += ')';
+    }else{
+        result += '(';
     }
 }
+return result;
 }
