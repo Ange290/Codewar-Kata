825f9404eaa1707e38fa00f504f971a88cd52c1b
Ange290 <uange209@gmail.com>
2026-01-29 17:35:47 +0200

    check for duplicate char

--- 6kyu/duplicateEncoder.js
+++ 6kyu/duplicateEncoder.js
@@ -1,6 +1,9 @@
 function duplicateEncoder(word){
 word = word.toLowercase();
+let result ='';
 for(let c= 0; c< word.length; c++){
-    
+    if(word.indexOf(word[c] !== word.lastIndexOf(word[c]))){
+
+    }
 }
 }
