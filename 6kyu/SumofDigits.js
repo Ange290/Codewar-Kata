0f41c60e482097ac9df649945a64a0cc39435dbd
Ange290 <uange209@gmail.com>
2026-02-02 11:30:43 +0200

    add how to find sum

--- 6kyu/SumofDigits.js
+++ 6kyu/SumofDigits.js
@@ -1,3 +1,8 @@
 function sumOfDigits(n){
-    
+    let sum = n;
+    while(sum > 9){
+        let digits = String(sum).split('').map(Number);
+        sum = digits.reduce((acc,num)=> acc + num, 0);
+    }
+    return sum
 }
