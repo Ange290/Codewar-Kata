ea18d28b4aea800ebef557e8cf84006430e80b1d
Ange290 <uange209@gmail.com>
2026-02-07 10:00:00 +0200

    add defaut value

--- 8kyu/simpleCalculator.ts
+++ 8kyu/simpleCalculator.ts
@@ -4,12 +4,14 @@ if(operator.includes(sign)){
     switch(sign){
         case '+':
             return a+b;
-        case '-':
+         case '-':
             return a-b;
         case '*':
             return a*b;
         case '/':
             return a/b;
+            default:
+            return 'unknown value';
     }
 }
 }
