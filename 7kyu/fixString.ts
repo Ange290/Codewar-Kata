dc583c4430fe538c3d98e159ef66c97844861d49
Ange290 <uange209@gmail.com>
2026-02-09 17:54:53 +0200

    Fixing string cases

---
+++ 7kyu/fixString.ts
@@ -0,0 +1,20 @@
+function changes (str:string): string{
+    let upper:string[] =[];
+    let lower:string[] =[];
+    for(let ch =0; ch< str.length; ch++){
+        if(str[ch]=== str[ch].toUpperCase()){
+            upper.push(str[ch]);
+        }else if(str[ch] ===str[ch].toLowerCase()){
+            lower.push(str[ch]);
+        }
+    }
+    let sumUpper:number = upper.join('').length;
+    let sumLower:number = lower.join('').length;
+    if(sumUpper > sumLower){
+       return str.toUpperCase();
+    }
+    else{
+        return str.toLowerCase();
+    }
+}
+console.log(changes("SOLve"));
