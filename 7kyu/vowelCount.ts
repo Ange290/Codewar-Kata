fcd198abf539207797518c0d04da52c4d44d5c38
Ange290 <uange209@gmail.com>
2026-02-11 20:15:30 +0200

    loop in string and check vowel

--- 7kyu/vowelCount.ts
+++ 7kyu/vowelCount.ts
@@ -2,4 +2,9 @@ export class Kata {
     static getCount(str: string): number { 
          let nn: number = 0;
     let vowel: string = 'aeiou';
+      for (let a = 0; a < str.length; a++) {
+      if (vowel.includes(str[a])) {
+        nn++;
+      }
+    }
         } }
