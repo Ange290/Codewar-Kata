43ce5a8570dc64c3add9bf4a9bfd27f7a50c487a
Ange290 <uange209@gmail.com>
2026-02-11 20:27:37 +0200

    increment each number

--- 7kyu/incrementer.ts
+++ 7kyu/incrementer.ts
@@ -2,5 +2,8 @@ export function incrementer(nums: number[]) : number[] {
      let arr: number[]=[];
     let incre : number =1;
      for(let a = 0; a< nums.length; a++){
+         let value : number = nums[a] + incre;
+  arr.push(value % 10);
+  incre++;
      }
 }
