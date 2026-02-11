92219894ded7a1f1c0b609d3df8e3740f7aaac00
Ange290 <uange209@gmail.com>
2026-02-11 20:28:26 +0200

    return and add test value

--- 7kyu/incrementer.ts
+++ 7kyu/incrementer.ts
@@ -6,4 +6,8 @@ export function incrementer(nums: number[]) : number[] {
   arr.push(value % 10);
   incre++;
      }
+     return arr;
 }
+console.log(incrementer([1,2,3]));
+console.log(incrementer([4,6,7,1,3]));
+console.log(incrementer([3,6,9,8,9]));
