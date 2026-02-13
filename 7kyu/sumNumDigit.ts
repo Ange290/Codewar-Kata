a3bb12be00cb409cb1dcc0b494b8d985dd27fb8e
Ange290 <uange209@gmail.com>
2026-02-13 10:00:00 +0200

    add return value

--- 7kyu/sumNumDigit.ts
+++ 7kyu/sumNumDigit.ts
@@ -2,4 +2,5 @@ const sumDigits=(number: number)=> {
     let sum =0;
      let turnArry = Array.from(String(Math.abs(number)),Number);
      sum= turnArry.reduce((a,b)=> a+b);
+        return sum;
 }
