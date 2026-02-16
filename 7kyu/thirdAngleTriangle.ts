90749b5e349c2bfbe1c8fa715c7023630a05dd90
Ange290 <uange209@gmail.com>
2026-02-16 17:29:49 +0200

    Add testing value

--- 7kyu/thirdAngleTriangle.ts
+++ 7kyu/thirdAngleTriangle.ts
@@ -2,3 +2,5 @@ export const otherAngle = (a: number, b: number): number => {
     let missingSide: number = 180- (a + b);
   return missingSide;
 }
+console.log(otherAngle(30,60));
+console.log(otherAngle(60,60));
