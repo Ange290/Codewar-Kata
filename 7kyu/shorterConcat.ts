5b8d23f8b7c142c0f38c449d9887968238f18eb1
Ange290 <uange209@gmail.com>
2026-02-20 10:00:00 +0200

    add first condition

--- 7kyu/shorterConcat.ts
+++ 7kyu/shorterConcat.ts
@@ -2,4 +2,9 @@ const shorterReverseLonger=(a:string, b:string) =>{
       let ac:number = a.length;
   let bc:number = b.length;
   let result;
+
+    if (bc < ac) {
+    let am = a.split('').reverse().join('');
+    result = `${b}${am}${b}`;
+  }
 }
