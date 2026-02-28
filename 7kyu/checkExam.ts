812e21a43f461eaddaebf84b3b960166e946778a
Ange290 <uange209@gmail.com>
2026-02-28 10:00:00 +0200

    check if answer or student are blank

--- 7kyu/checkExam.ts
+++ 7kyu/checkExam.ts
@@ -3,5 +3,8 @@ const checkExam = (array1: any, array2: any) => {
     for(let i =0; i< array1.length; i++){
   let answer = array1[i];
   let student = array2[i];
+
+   if(answer === "" || student === "") {
+     sum += 0;//blank atleast in one element
     }
 }
