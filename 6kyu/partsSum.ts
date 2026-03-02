93eb9f50adc51b30135b9ec003108d459834733a
Ange290 <uange209@gmail.com>
2026-03-02 19:47:22 +0200

    repeat until arr equal to zero

--- 6kyu/partsSum.ts
+++ 6kyu/partsSum.ts
@@ -2,4 +2,8 @@ export function partsSums(ls: number[]): number[] {
 let sums: number[] = [];
 
   let total = ls.reduce((a, b) => a + b, 0);
+  for (let i = 0; i < ls.length; i++) {
+    sums.push(total);
+    total -= ls[i];
+  }
 }
