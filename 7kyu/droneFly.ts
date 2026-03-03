f92c1d39abf187a2ce6034c785c7d942148230f9
Ange290 <uange209@gmail.com>
2026-03-03 20:07:07 +0200

    Loop to check indexes

--- 7kyu/droneFly.ts
+++ 7kyu/droneFly.ts
@@ -3,4 +3,11 @@ export function flyBy(lamps: string, drone: string): string {
  let drones: number = drone.length;
    let res :string[]= [];
    let not :string[] =[];
+
+    let lamp= lamps.split('');
+  for(let x=0; x<lamp.length; x++){
+      if(x < drones ){
+         res.push(lamp[x].replace(lamp[x],'o'));
+      }
+    }
 }
