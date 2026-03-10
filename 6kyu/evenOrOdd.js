adf221de55eb9fed2770b9596c9e8dc1ddaff460
Ange290 <uange209@gmail.com>
2026-03-10 21:09:56 +0200

    Use proxy to handle error

--- 6kyu/evenOrOdd.js
+++ 6kyu/evenOrOdd.js
@@ -15,3 +15,29 @@ console.log(evenOrOdd(7)); //'Odd'
 console.log(evenOrOdd[7]); //'Odd'
 
 //Way to handle all number negative or positive regular number or array 
+const evenOrOdd = new Proxy(
+  function (n) {
+   
+    return n % 2 === 0 ? "Even" : "Odd";
+  },
+  {
+    get(target, prop) {
+     
+      const num = Number(prop);
+      if (!isNaN(num)) {
+        return num % 2 === 0 ? "Even" : "Odd";
+      }
+      return target[prop];
+    },
+  }
+);
+
+
+console.log(evenOrOdd(2));          // Even
+console.log(evenOrOdd[2]);          // Even
+console.log(evenOrOdd(7));          // Odd
+console.log(evenOrOdd[7]);          // Odd
+console.log(evenOrOdd[-44]);        // Even
+console.log(evenOrOdd[-1416990353165065]); // Odd
+console.log(evenOrOdd[1781651586945717]);  // Odd
+console.log(evenOrOdd[-1158994119452252]); // Even
