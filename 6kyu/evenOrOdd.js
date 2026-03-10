437e84f343629aed894ad2c78003924db5833a73
Ange290 <uange209@gmail.com>
2026-03-10 21:08:01 +0200

    Add testing value

--- 6kyu/evenOrOdd.js
+++ 6kyu/evenOrOdd.js
@@ -8,3 +8,8 @@ for (let i = 0; i < 1000; i++) {
 for (let i = 0; i > -1000; i--) {
   evenOrOdd[i] = i % 2 === 0 ? "Even" : "Odd";
 }
+
+console.log(evenOrOdd(2)); //'Even'
+console.log(evenOrOdd[2]); //'Even'
+console.log(evenOrOdd(7)); //'Odd'
+console.log(evenOrOdd[7]); //'Odd'
