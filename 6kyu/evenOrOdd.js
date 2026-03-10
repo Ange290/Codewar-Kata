1f3b9951bf7e2219304f20351db605720ae736f6
Ange290 <uange209@gmail.com>
2026-03-10 21:06:55 +0200

    when array in negative

--- 6kyu/evenOrOdd.js
+++ 6kyu/evenOrOdd.js
@@ -5,3 +5,6 @@ function evenOrOdd(n) {
 for (let i = 0; i < 1000; i++) {
   evenOrOdd[i] = i % 2 === 0 ? "Even" : "Odd";
 }
+for (let i = 0; i > -1000; i--) {
+  evenOrOdd[i] = i % 2 === 0 ? "Even" : "Odd";
+}
