9724fd72ea1a81b1f34ccc53e4bff84fcfc8355c
Ange290 <uange209@gmail.com>
2026-03-11 15:10:13 +0200

    Check is array is empty

---
+++ 6kyu/stalinSort.ts
@@ -0,0 +1,9 @@
+export function stalinSort(array: number[]): void {
+    if(array.length === 0) return;
+ for(let x =0; x < array.length;){
+    if(array[x] >= array[x + 1]){
+    
+    }
+ }
+}
+stalinSort([5, 3, 1, 2, 4])
