eab88c9e38f44c2482f412d7cb6b3265cc5c1695
Ange Uwizera <154429400+Ange290@users.noreply.github.com>
2026-03-17 09:17:04 +0200

    Implement character value summation in wordValue
    Calculate the sum of character values for each word.

--- 7kyu/wordValue.ts
+++ 7kyu/wordValue.ts
@@ -2,7 +2,13 @@
 export function wordValue(arr: string[]): number[] {
   return arr.map((word, index) => {
     let sum = 0;
+    for (let char of word) {
+      if (char !== " ") {
+        sum += char.charCodeAt(0) - 96;
+      }
+    }
 
+  
 
   });
 }
