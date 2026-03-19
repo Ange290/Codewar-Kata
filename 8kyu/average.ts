ceac63065a520a719027b7ea8d36e004197d73bc
Ange Uwizera <154429400+Ange290@users.noreply.github.com>
2026-03-19 17:22:51 +0200

    Return floored average of array elements

--- 8kyu/average.ts
+++ 8kyu/average.ts
@@ -4,6 +4,8 @@ export function findAverage(array: number[]): number {
   if(array.length === 0)return 0;
  let sum :number = array.reduce((a,b) => a +b);
 
+  return Math.floor(sum/array.length);
 }
+
 
 
