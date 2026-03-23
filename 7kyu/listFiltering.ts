e5f9c972365e1f149ebbf2aa27e37043933b480c
Ange Uwizera <154429400+Ange290@users.noreply.github.com>
2026-03-23 20:02:40 +0200

    Add filtering logic to return only numbers from list
    Implement filtering logic to return only numbers.

--- 7kyu/listFiltering.ts
+++ 7kyu/listFiltering.ts
@@ -2,5 +2,6 @@ export function filter_list(l:Array<any>):Array<number> {
   // Return a new array with the strings filtered out
      let result:number[] =[];
     for(let a = 0; a < l.length; a++){
-    }
+        if(typeof l[a] == 'number'){
+        }}
 }
