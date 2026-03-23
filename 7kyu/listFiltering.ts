ca410e2d2f55032e1d6c510df1e1b19abe8d2b84
Ange Uwizera <154429400+Ange290@users.noreply.github.com>
2026-03-23 20:02:02 +0200

    Add filtering logic to filter_list function
    Implement filtering logic to remove strings from the list.

--- 7kyu/listFiltering.ts
+++ 7kyu/listFiltering.ts
@@ -1,5 +1,6 @@
 export function filter_list(l:Array<any>):Array<number> {
   // Return a new array with the strings filtered out
      let result:number[] =[];
-   
+    for(let a = 0; a < l.length; a++){
+    }
 }
