2a6d26dd85e219c39f7434784dda9c94b9530227
Ange Uwizera <154429400+Ange290@users.noreply.github.com>
2026-03-26 20:18:26 +0200

    Calculate products of adjacent elements in array
    Fix loop to calculate products of adjacent elements.

--- 7kyu/maxProduct.ts
+++ 7kyu/maxProduct.ts
@@ -3,6 +3,9 @@ export function adjacentElementsProduct(arr: number[]): number {
   let hold:number[] =[];
   for(let x =0; x< arr.length; x++){
       let check = arr[x] * arr[x+1]
+        hold.push(check);
   }
+  hold.pop()
+ 
 
 }
