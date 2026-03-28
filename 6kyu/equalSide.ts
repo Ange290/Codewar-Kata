7b9636fc9199b1c02ef55d1735b8f21a5268318b
Ange290 <uange209@gmail.com>
2026-03-28 10:00:00 +0200

    add testing value

--- 6kyu/equalSide.ts
+++ 6kyu/equalSide.ts
@@ -8,3 +8,6 @@ const findEvenIndex =(arr:number[])=>{
     }
     return -1
 }
+console.log(findEvenIndex([1,2,3,4,3,2,1]));
+console.log(findEvenIndex([1,100,50,-51,1,1]));
+console.log(findEvenIndex([1,2,3,4,5,6]));
