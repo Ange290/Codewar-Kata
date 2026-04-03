110c202ee77487e9596aaa8d5caf777a2ce5b2d5
Ange290 <uange209@gmail.com>
2026-04-03 10:00:00 +0200

    add testing value

--- 7kyu/getMiddleChar.ts
+++ 7kyu/getMiddleChar.ts
@@ -2,3 +2,7 @@ const getMiddle = (s: string) => {
     const middle = Math.floor(s.length / 2);
     return s.length % 2 === 1 ? s[middle] : s.slice(middle - 1, middle + 1);
 };
+console.log(getMiddle('test')); // 'es'
+console.log(getMiddle('testing'));
+console.log(getMiddle('middle'));
+console.log(getMiddle('A'));
