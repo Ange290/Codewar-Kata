82e3eeefa1eaedcfc353e285fcf2aecf22b3863e
Ange290 <uange209@gmail.com>
2026-04-06 10:00:00 +0200

    add test values

--- 7kyu/runOutSpace.ts
+++ 7kyu/runOutSpace.ts
@@ -2,3 +2,6 @@ function spacey(array: string[]): string[] {
 	let string = ''
 	return array.map( (e) => string += e );
 }
+console.log(spacey(['kevin', 'has', 'no', 'space']));
+console.log(spacey(['this', 'is', 'a', 'sentence']));
+console.log(spacey(['lack', 'of', 'space']));
