e0bf18d0d102302b39f8189654975112799b4b5c
Ange290 <uange209@gmail.com>
2026-04-13 10:00:00 +0200

    check if num is module to i

--- 6kyu/isNumPrime.ts
+++ 6kyu/isNumPrime.ts
@@ -2,5 +2,7 @@ function isPrime(num: any) {
     if (num<=1){
         return false;
     }
-    for(let i=2; i<=Math.sqrt(num); i++){}
+    for(let i=2; i<=Math.sqrt(num); i++){
+        if(num % i === 0){}
+    }
 }
