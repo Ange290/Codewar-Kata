cc584e5e9a76a498348cc93cd3a6fa60a9e5b351
Ange290 <uange209@gmail.com>
2026-04-30 15:49:13 +0200

    Increase count according to condition

--- 6kyu/findNumArray.ts
+++ 6kyu/findNumArray.ts
@@ -1,7 +1,9 @@
 export function countNumber(matrix: number[],num: number): number{
     let count = 0;
     for(let i = 0; i < matrix.length; i++){
-      
+        if(matrix[i] === num){
+            count++;
+        }
     }
    
 }
