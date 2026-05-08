7b5023d505abf5478a7d6747995d885a9cdbd1d7
Ange290 <uange209@gmail.com>
2026-05-08 10:00:00 +0200

    add value to result variable

--- 6kyu/uniqueOrder.ts
+++ 6kyu/uniqueOrder.ts
@@ -1,6 +1,8 @@
 const uniqueInOrder = function(iterable: any){
     let result: any[] = [];
     for(let i=0; i<iterable.length; i++){
-        if(iterable[i] !== iterable[i+1]){}
+        if(iterable[i] !== iterable[i+1]){
+            result.push(iterable[i]);
+        }
     }
 }
