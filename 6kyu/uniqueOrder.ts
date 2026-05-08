df469256bed79bc4ced7f4e146144c6e223e540b
Ange290 <uange209@gmail.com>
2026-05-08 10:00:00 +0200

    checking values

--- 6kyu/uniqueOrder.ts
+++ 6kyu/uniqueOrder.ts
@@ -1,4 +1,6 @@
 const uniqueInOrder = function(iterable: any){
     let result: any[] = [];
-    for(let i=0; i<iterable.length; i++){}
+    for(let i=0; i<iterable.length; i++){
+        if(iterable[i] !== iterable[i+1]){}
+    }
 }
