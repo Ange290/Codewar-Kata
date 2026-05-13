3be0910106f014e2815d6d698f15a14a7ae292ac
Ange290 <uange209@gmail.com>
2026-05-13 10:00:00 +0200

    looping to find column

--- 6kyu/multiplicationTable.ts
+++ 6kyu/multiplicationTable.ts
@@ -3,6 +3,9 @@ function multiplicationTable(size: number): number[][] {
      let table= [];
  for(let a=1; a<=size;a++){
     let row =[];
+     for(let b=1; b<=size; b++){
+      row.push(a*b);
+    }
  }
 
 }
