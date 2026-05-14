195891c73f05d83133aa179a28e2aade4193bc9f
Ange290 <uange209@gmail.com>
2026-05-14 10:00:00 +0200

    checking if value match

--- 6kyu/findMissingLetter.ts
+++ 6kyu/findMissingLetter.ts
@@ -3,5 +3,9 @@ function findMissingLetter(array: string[]): string {
        for (let i = 0; i < array.length - 1; i++) {
         const currentCode = array[i].charCodeAt(0);
         const nextCode = array[i + 1].charCodeAt(0);
+
+          if (nextCode - currentCode > 1) {
+            return String.fromCharCode(currentCode + 1);
+        }
        }
 }
