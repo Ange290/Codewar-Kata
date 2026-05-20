654915527ac6034c469e601794ccffeab38aeedc
Ange290 <uange209@gmail.com>
2026-05-20 15:05:14 +0200

    Checking numbers

--- 6kyu/twoSum.js
+++ 6kyu/twoSum.js
@@ -2,5 +2,10 @@ function twoSum(numbers, target) {
   let indx =[];
   for(let x=0; x<numbers.length; x++){
     for(let y= x+1; y<numbers.length; y++){
-    
-    }}}
+        if(numbers[x] + numbers[y] === target){
+         
+        }
+    }
+    }
+}
+  
