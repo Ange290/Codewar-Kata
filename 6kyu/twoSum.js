5c1182ef149f8c806b4ce4cc161cb270aba62167
Ange290 <uange209@gmail.com>
2026-05-20 15:05:35 +0200

    Add value i indx variable

--- 6kyu/twoSum.js
+++ 6kyu/twoSum.js
@@ -3,9 +3,9 @@ function twoSum(numbers, target) {
   for(let x=0; x<numbers.length; x++){
     for(let y= x+1; y<numbers.length; y++){
         if(numbers[x] + numbers[y] === target){
-         
+            indx.push(x);
+            indx.push(y);
         }
     }
     }
 }
-  
